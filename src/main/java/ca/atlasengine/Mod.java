7269acbc357610a792ce610107454bcabf2550ba
iam4722202468 <aceparent@gmail.com>
2023-06-25 20:27:55 -0400

    1.20.1

--- src/main/java/ca/atlasengine/Mod.java
+++ src/main/java/ca/atlasengine/Mod.java
@@ -1,20 +1,49 @@
 package ca.atlasengine;
 
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
+import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
 
 public class Mod implements ModInitializer {
-    public static final ItemGroup ATLASENGINE_LOGO = FabricItemGroupBuilder
-            .build(new Identifier("atlasengine", "logo"), () -> new ItemStack(ModItems.LOGO));
+    public static final ItemGroup ATLASENGINE_LOGO = FabricItemGroup.builder()
+            .displayName(Text.translatable("itemGroup.atlasengine.logo"))
+            .icon(() -> new ItemStack(ModItems.LOGO))
+            .build();
 
-    public static final ItemGroup ATLASENGINE_MODEL = FabricItemGroupBuilder
-            .build(new Identifier("atlasengine", "model"), () -> new ItemStack(ModItems.MODEL));
+    public static final ItemGroup ATLASENGINE_MODEL = FabricItemGroup.builder()
+            .displayName(Text.translatable("itemGroup.atlasengine.model"))
+            .icon(() -> new ItemStack(ModItems.MODEL))
+            .build();
 
     @Override
     public void onInitialize() {
         ModItems.registerModItems();
+
+        var LOGO = RegistryKey.of(RegistryKeys.ITEM_GROUP, new Identifier("atlasengine", "logo"));
+        var MODEL = RegistryKey.of(RegistryKeys.ITEM_GROUP, new Identifier("atlasengine", "model"));
+
+        Registry.register(Registries.ITEM_GROUP,
+                LOGO,
+                ATLASENGINE_LOGO);
+
+        Registry.register(Registries.ITEM_GROUP,
+                MODEL,
+                ATLASENGINE_MODEL);
+
+        ItemGroupEvents.modifyEntriesEvent(LOGO).register(content -> {
+            content.add(ModItems.LOGO);
+        });
+
+        ItemGroupEvents.modifyEntriesEvent(MODEL).register(content -> {
+            content.add(ModItems.MODEL);
+        });
     }
 }
--- src/main/java/ca/atlasengine/ModItems.java
+++ src/main/java/ca/atlasengine/ModItems.java
@@ -2,18 +2,19 @@ package ca.atlasengine;
 
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.minecraft.item.Item;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
-import net.minecraft.util.registry.Registry;
 
 public class ModItems {
     public static final Item LOGO = registerItem("logo",
-            new Item(new FabricItemSettings().group(Mod.ATLASENGINE_LOGO)));
+            new Item(new FabricItemSettings()));
 
     public static final Item MODEL = registerItem("model",
-            new Item(new FabricItemSettings().group(Mod.ATLASENGINE_MODEL)));
+            new Item(new FabricItemSettings()));
 
     private static Item registerItem(String name, Item item) {
-        return Registry.register(Registry.ITEM, new Identifier("atlasengine", name), item);
+        return Registry.register(Registries.ITEM, new Identifier("atlasengine", name), item);
     }
 
     public static void registerModItems() {
